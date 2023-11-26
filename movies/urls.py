9a7ed6b1448034f0cbdab475e2c5e2edd25fc2a2
Deshy Dan <deshy733@gmail.com>
2023-11-26 22:38:00 +0200

    Implemented url

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="movies_index")
+
+    path('<int:movie_id>', view.detail, name="movies.detail")
 ]
--- movies/views.py
+++ movies/views.py
@@ -5,3 +5,10 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     render(request, 'movies/index.html', {'movies', movies})
+
+
+
+def detail(request , movie_id):
+
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movie/detail.html', {'movie', movie})
